f677b39cb60b36d7408fea4dbcbd4ebb19a2d5fe
strivervishal <strivervishal@gmail.com>
2025-03-05 15:34:32 +0530

    malti code

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import DashboardCards from './DashboardCards'
+import Sidebar from './Sidebar'
 
 const Dashboard = () => {
   return (
     <div>Dashboard
+      <Sidebar />
       <DashboardCards />
     </div>
   )
