25ce9589814d20e8cd5743e42e0160a4e6e97325
strivervishal <strivervishal@gmail.com>
2025-03-05 21:15:58 +0530

    bhakti code

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import DashboardCards from './DashboardCards'
 import Sidebar from './Sidebar'
 import BookedAppointments from './BookedAppointments'
+import Navbar from './Navbar'
 
 const Dashboard = () => {
   return (
     <div>Dashboard
+      <Navbar />
       <Sidebar />
       <DashboardCards />
       <BookedAppointments />
