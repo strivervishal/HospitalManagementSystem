fdbbed843f36345aec1d242de79b6a4aacea810e
Rajesh Bugga <110104629+buggarajesh@users.noreply.github.com>
2025-03-16 08:37:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const session = require("express-session");
 const User = require("./models/user");
 
 const doctorRoutes = require("./routes/doctorRoutes");
-//const dashboardCardRoutes = require("/routes/dashboardcards"); 
+const dashboardCardRoutes = require("/routes/dashboardcards"); 
 const patientRoutes = require("./routes/patientRoutes");
 const appointmentRoutes = require("./routes/appointment");
 const authRoutes = require("./routes/auth");
@@ -65,7 +65,7 @@ connectDB();
 
 app.use("/patients", patientRoutes);
 app.use("/appointments", appointmentRoutes);
-//app.use("/api/dashboardcards", dashboardCardRoutes);
+app.use("/api/dashboardcards", dashboardCardRoutes);
 app.use("/auth", authRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/medicines", medicineRoutes);
